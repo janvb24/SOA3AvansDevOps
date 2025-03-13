09f8e283099a7d535432603746ccf7afd34146e8
J.T.B. van Berkel <118279854+janvb24@users.noreply.github.com>
2025-03-13 16:00:40 +0100

    Added interfaces

---
+++ AvansDevops/AvansDevops/SoftwareConfigurationManagement/GitVersionControl.cs
@@ -0,0 +1,19 @@
+﻿namespace AvansDevops.SoftwareConfigurationManagement {
+    public class GitVersionControl : IGitVersionControl {
+        public void Branch(string branchName) {
+            throw new NotImplementedException();
+        }
+
+        public void Commit(string message) {
+            throw new NotImplementedException();
+        }
+
+        public void Fetch() {
+            throw new NotImplementedException();
+        }
+
+        public void Push() {
+            throw new NotImplementedException();
+        }
+    }
+}
