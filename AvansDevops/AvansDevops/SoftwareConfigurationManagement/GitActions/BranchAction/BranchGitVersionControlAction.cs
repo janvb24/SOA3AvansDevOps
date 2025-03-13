94078b9cf8b93655a53f6517b2c0f31963dbe741
J.T.B. van Berkel <118279854+janvb24@users.noreply.github.com>
2025-03-13 16:06:02 +0100

    Implemented the interfaces

---
+++ AvansDevops/AvansDevops/SoftwareConfigurationManagement/GitActions/BranchAction/BranchGitVersionControlAction.cs
@@ -0,0 +1,7 @@
+﻿namespace AvansDevops.SoftwareConfigurationManagement.GitActions.BranchAction {
+    public class BranchGitVersionControlAction : IBranchGitVersionControlAction {
+        public void Branch(string branchName) {
+            Console.WriteLine($"Branching to branch {branchName}");
+        }
+    }
+}
--- AvansDevops/AvansDevops/SoftwareConfigurationManagement/GitVersionControl.cs
+++ AvansDevops/AvansDevops/SoftwareConfigurationManagement/GitVersionControl.cs
@@ -1,19 +1,36 @@
-﻿namespace AvansDevops.SoftwareConfigurationManagement {
+﻿using AvansDevops.SoftwareConfigurationManagement.GitActions.BranchAction;
+using AvansDevops.SoftwareConfigurationManagement.GitActions.CommitAction;
+using AvansDevops.SoftwareConfigurationManagement.GitActions.FetchAction;
+using AvansDevops.SoftwareConfigurationManagement.GitActions.PushAction;
+
+namespace AvansDevops.SoftwareConfigurationManagement {
     public class GitVersionControl : IGitVersionControl {
+        private readonly IBranchGitVersionControlAction branchGitVersionControlAction;
+        private readonly ICommitGitVersionControlAction commitGitVersionControlAction;
+        private readonly IFetchGitVersionControlAction fetchGitVersionControlAction;
+        private readonly IPushGitVersionControlAction pushGitVersionControlAction;
+
+        public GitVersionControl(IBranchGitVersionControlAction branchGitVersionControlAction, ICommitGitVersionControlAction commitGitVersionControlAction, IFetchGitVersionControlAction fetchGitVersionControlAction, IPushGitVersionControlAction pushGitVersionControlAction) {
+            this.branchGitVersionControlAction = branchGitVersionControlAction;
+            this.commitGitVersionControlAction = commitGitVersionControlAction;
+            this.fetchGitVersionControlAction = fetchGitVersionControlAction;
+            this.pushGitVersionControlAction = pushGitVersionControlAction;
+        }
+
         public void Branch(string branchName) {
-            throw new NotImplementedException();
+            branchGitVersionControlAction.Branch(branchName);
         }
 
         public void Commit(string message) {
-            throw new NotImplementedException();
+            commitGitVersionControlAction.Commit(message);
         }
 
         public void Fetch() {
-            throw new NotImplementedException();
+            fetchGitVersionControlAction.Fetch();
         }
 
         public void Push() {
-            throw new NotImplementedException();
+            pushGitVersionControlAction.Push();
         }
     }
 }
