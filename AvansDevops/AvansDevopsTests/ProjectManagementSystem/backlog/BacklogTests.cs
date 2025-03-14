d576cb3efc803f06d21b6c98d3f2eba49c8538e3
J.T.B. van Berkel <118279854+janvb24@users.noreply.github.com>
2025-03-14 10:08:20 +0100

    Added tests

---
+++ AvansDevops/AvansDevopsTests/ProjectManagementSystem/backlog/BacklogTests.cs
@@ -0,0 +1,46 @@
+﻿using AvansDevops.ProjectManagementSystem.backlog;
+
+namespace AvansDevopsTests.ProjectManagementSystem.backlog {
+    public class BacklogTests {
+        [Fact]
+        public void TestAddBacklogItem() {
+            // Arrange
+            var backlog = new Backlog();
+            var backlogItem = new EditableBacklogItem("Test", 5);
+            
+            // Act
+            backlog.AddBacklogItem(backlogItem);
+            
+            // Assert
+            Assert.Contains(backlogItem, backlog.GetBacklogItems());
+        }
+
+        [Fact]
+        public void TestRemoveBacklogItem() {
+            // Arrange
+            var backlog = new Backlog();
+            var backlogItem = new EditableBacklogItem("Test", 5);
+            backlog.AddBacklogItem(backlogItem);
+            
+            // Act
+            backlog.RemoveBacklogItem(backlogItem);
+            
+            // Assert
+            Assert.DoesNotContain(backlogItem, backlog.GetBacklogItems());
+        }
+
+        [Fact]
+        public void TestGetBacklogItems() {
+            // Arrange
+            var backlog = new Backlog();
+            var backlogItem = new EditableBacklogItem("Test", 5);
+            backlog.AddBacklogItem(backlogItem);
+            
+            // Act
+            var backlogItems = backlog.GetBacklogItems();
+            
+            // Assert
+            Assert.Contains(backlogItem, backlogItems);
+        }
+    }
+}
