409acbd16cd49f81b70f602d882c389ec44af216
TijnVanBekhoven <113771373+TijnVanBekhoven@users.noreply.github.com>
2025-03-27 14:12:16 +0100

    Added losts tests in ProjectTests.cs

--- AvansDevops/AvansDevopsTests/ProjectManagementSystem/ProjectTests.cs
+++ AvansDevops/AvansDevopsTests/ProjectManagementSystem/ProjectTests.cs
@@ -1,6 +1,7 @@
 ﻿using AvansDevops.DevOps;
 using AvansDevops.DevOps.DeployActions;
 using AvansDevops.ProjectManagementSystem;
+using AvansDevops.ProjectManagementSystem.backlog;
 using AvansDevops.ProjectManagementSystem.sprint;
 using AvansDevops.SoftwareConfigurationManagement;
 
@@ -118,4 +119,103 @@ public class ProjectTests {
         // Assert
         Assert.Equivalent(expected, result, true);
     }
+    
+    [Fact]
+    public void MovingAnExistingBacklogItemShouldWork()
+    {
+        // Arrange
+        var gitVersionControl = Substitute.For<IGitVersionControl>();
+        List<User> developers = [new User("", "", "")];
+        var tester = new User("", "", "");
+        var leadDev = new User("", "", "");
+        var productOwner = new User("", "", "");
+        var project = new Project(gitVersionControl, developers, tester, leadDev, productOwner);
+        var scrumMaster = new User("name", "email@server.com", "0612345678");
+        var backlogItem = new BacklogItemMock("backlog item", 0, tester, scrumMaster);
+        var pipeline = Substitute.For<IPipeline>();
+
+        // Act
+        project.NewSprint(scrumMaster, pipeline, "new sprint", SprintType.REVIEW_SPRINT);
+        project.projectBacklog.AddBacklogItem(backlogItem);
+        project.MoveBacklogItemToSprint(backlogItem);
+
+        // Assert
+        Assert.Empty(project.projectBacklog.GetBacklogItems());
+        Assert.Equal(backlogItem, project.currentSprint.GetBacklogItems()[0]);
+    }
+
+    [Fact]
+    public void MovingANonExistentBacklogItemShouldThrowError()
+    {
+        // Arrange
+        var gitVersionControl = Substitute.For<IGitVersionControl>();
+        List<User> developers = [new User("", "", "")];
+        var tester = new User("", "", "");
+        var leadDev = new User("", "", "");
+        var productOwner = new User("", "", "");
+        var project = new Project(gitVersionControl, developers, tester, leadDev, productOwner);
+        var scrumMaster = new User("name", "email@server.com", "0612345678");
+        var backlogItem = new BacklogItemMock("backlog item", 0, tester, scrumMaster);
+        var pipeline = Substitute.For<IPipeline>();
+        
+        // Act
+        project.NewSprint(scrumMaster, pipeline, "new sprint", SprintType.REVIEW_SPRINT);
+        
+        // Assert
+        Assert.Throws<ArgumentException>(() => project.MoveBacklogItemToSprint(backlogItem));
+    }
+
+    [Fact]
+    public void AddingANonExistentBacklogItemToSprintShouldWork()
+    {
+        // Arrange
+        var gitVersionControl = Substitute.For<IGitVersionControl>();
+        List<User> developers = [new User("", "", "")];
+        var tester = new User("", "", "");
+        var leadDev = new User("", "", "");
+        var productOwner = new User("", "", "");
+        var project = new Project(gitVersionControl, developers, tester, leadDev, productOwner);
+        var scrumMaster = new User("name", "email@server.com", "0612345678");
+        var backlogItem = new BacklogItemMock("backlog item", 0, tester, scrumMaster);
+        var pipeline = Substitute.For<IPipeline>();
+        
+        // Act
+        project.NewSprint(scrumMaster, pipeline, "new sprint", SprintType.REVIEW_SPRINT);
+        project.AddNewBacklogItemToSprint(backlogItem);
+        
+        // Assert
+        Assert.Equal(backlogItem, project.currentSprint.GetBacklogItems()[0]);
+    }
+    
+    [Fact]
+    public void AddingAnExistingBacklogItemToSprintShouldThrowError()
+    {
+        // Arrange
+        var gitVersionControl = Substitute.For<IGitVersionControl>();
+        List<User> developers = [new User("", "", "")];
+        var tester = new User("", "", "");
+        var leadDev = new User("", "", "");
+        var productOwner = new User("", "", "");
+        var project = new Project(gitVersionControl, developers, tester, leadDev, productOwner);
+        var scrumMaster = new User("name", "email@server.com", "0612345678");
+        var backlogItem = new BacklogItemMock("backlog item", 0, tester, scrumMaster);
+        var pipeline = Substitute.For<IPipeline>();
+        
+        // Act
+        project.projectBacklog.AddBacklogItem(backlogItem);
+        project.NewSprint(scrumMaster, pipeline, "new sprint", SprintType.REVIEW_SPRINT);
+        
+        // Assert
+        Assert.Throws<ArgumentException>(() => project.AddNewBacklogItemToSprint(backlogItem));
+    }
+    
+    private class BacklogItemMock(
+        string title,
+        int storyPoints,
+        User tester,
+        User scrumMaster,
+        User? developer = null,
+        List<BacklogItem>? subTasks = null,
+        BacklogItem? parent = null)
+        : BacklogItem(title, storyPoints, developer, tester, scrumMaster, subTasks, parent);
 }
