ef2bd5656ad4a15f09e9f7cd2c40db45e165c9a5
TijnVanBekhoven <113771373+TijnVanBekhoven@users.noreply.github.com>
2025-03-28 16:48:02 +0100

    Added pdf summary to the review sprint.

--- AvansDevops/AvansDevops/ProjectManagementSystem/sprint/ReviewSprint.cs
+++ AvansDevops/AvansDevops/ProjectManagementSystem/sprint/ReviewSprint.cs
@@ -2,5 +2,25 @@
 
 namespace AvansDevops.ProjectManagementSystem.sprint;
 
-public class ReviewSprint(Project project, User scrumMaster, Pipeline pipeline, string name) : 
-    Sprint(project, scrumMaster, pipeline, name);
+public class ReviewSprint(Project project, User scrumMaster, Pipeline pipeline, string name) :
+    Sprint(project, scrumMaster, pipeline, name)
+{
+    public bool summaryAdded { get; private set; }
+    
+    /// <summary>
+    /// Saves a summary as PDF
+    /// </summary>
+    /// <param name="fileLocation">File location of PDF to be saved</param>
+    /// <exception cref="FileLoadException">Thrown when file is not a PDF</exception>
+    public void SaveSummaryPdf(string fileLocation)
+    {
+        var fileExtension = fileLocation.Split(".").Last();
+        if (!fileExtension.Equals("pdf")) throw new FileLoadException("File type should be pdf");
+
+        var fileName = fileLocation.Split("\\").Last();
+        var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+        File.WriteAllText(filePath, "Dummy text...");
+        summaryAdded = true;
+        Console.WriteLine($"{fileName} saved");
+    }
+}
